af58d7c6a7bc05c2b13da7c5b4ac98bc974f7722
Joe Richardson <hello.joer@gmail.com>
2018-12-31 21:21:25 +0000

    chore(cron) add cron job

--- index.js
+++ index.js
@@ -3,42 +3,47 @@ require("dotenv").config();
 // Imports
 const http = require("http");
 const fetch = require("node-fetch");
-var Twitter = require("twitter");
+const twitter = require("twitter");
+const cron = require("node-cron");
+
 
 // Node
 const hostname = "127.0.0.1";
 const port = 3000;
 
 // Twitter
-const client = new Twitter({
+const client = new twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+// Weather
+const API_KEY = process.env.WEATHER_API_KEY;
+const LOCATION = process.env.WEATHER_LOCATION;
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&APPID=${API_KEY}&units=metric`;
+
 const server = http.createServer((req, res) => {
-  const API_KEY = process.env.WEATHER_API_KEY;
-  const LOCATION = process.env.WEATHER_LOCATION;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&APPID=${API_KEY}&units=metric`;
+  cron.schedule('0 0 */3 * * *', function () {
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(weather) {
+        const output = `Currently in Leicester the tempature is ${weather.main.temp} with a high of ${
+          weather.main.temp_min
+        } and a low of ${weather.main.temp_max}. #${weather.weather[0].main} #Weather`;
 
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(weather) {
-      const output = `Currently in Leicester the tempature is ${weather.main.temp} with a high of ${
-        weather.main.temp_min
-      } and a low of ${weather.main.temp_max}. #${weather.weather[0].main} #Weather`;
-
-      const params = { screen_name: 'nodejs' };
-      client.post('statuses/update', {status: output}, function(error, tweet, response) {
-        if (!error) {
-          console.log(tweet);
-        }
+        const params = { screen_name: "nodejs" };
+        client.post("statuses/update", { status: output }, function(error, tweet, response) {
+          if (!error) {
+            console.log(tweet);
+          }
+        });
+        res.end(output);
       });
-      res.end(output);
-    });
+  });
 });
 
 server.listen(port, hostname, () => {
