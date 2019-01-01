237823a19bcae3e6934d9f602c7b2b3dcdc69200
Joe Richardson <hello.joer@gmail.com>
2019-01-01 18:54:31 +0000

    fix(node) remove node server

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 require("dotenv").config();
 
 // Imports
-const http = require("http");
 const fetch = require("node-fetch");
 const twitter = require("twitter");
-const cron = require("node-cron");
-
-
-// Node
-const hostname = "127.0.0.1";
-const port = 3000;
 
 // Twitter
 const client = new twitter({
@@ -24,28 +17,21 @@ const API_KEY = process.env.WEATHER_API_KEY;
 const LOCATION = process.env.WEATHER_LOCATION;
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&APPID=${API_KEY}&units=metric`;
 
-const server = http.createServer((req, res) => {
-  cron.schedule('* * * * * *', function () {
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(weather) {
-        const output = `Currently in Leicester the tempature is ${weather.main.temp} with a high of ${
-          weather.main.temp_min
-        } and a low of ${weather.main.temp_max}. #${weather.weather[0].main} #Weather`;
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(weather) {
+    const output = `Currently in ${LOCATION} the tempature is ${weather.main.temp} with a high of ${
+      weather.main.temp_min
+    } and a low of ${weather.main.temp_max}. #${weather.weather[0].main} #Weather`;
 
-        const params = { screen_name: "nodejs" };
-        client.post("statuses/update", { status: output }, function(error, tweet, response) {
-          if (!error) {
-            console.log(tweet);
-          }
-        });
-        res.end(output);
-      });
+    console.log(output);
+
+    const params = { screen_name: "nodejs" };
+    client.post("statuses/update", { status: output }, function(error, tweet, response) {
+      if (!error) {
+        console.log(tweet);
+      }
+    });
   });
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
